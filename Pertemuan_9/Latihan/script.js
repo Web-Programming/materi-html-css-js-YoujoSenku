1caa79cc0ee8bbe2dd289b11e02116365f0687a8
YoujoSenku <scl491455@gmail.com>
2026-03-31 18:07:09 +1200

    Perbaikan

--- Pertemuan_9/Latihan/script.js
+++ Pertemuan_9/Latihan/script.js
@@ -1,46 +1,42 @@
-//Fungsi untuk memuat data dari session storage dan menampilkannya
-function displayGuest(){
-    const guestListUI = document.getElementById('guestList');
-    guestListUI.innerHTML = ''; // Reset tampilan list
+//fungsi untuk memuat data dari session storage dan menampilkannya
+function displayGuests(){
+    const guestListUI = document.getElementById("guestList");
+    guestListUI.innerHTML = ''; //Reset tampilan list
 
-    //Mengambil data dari session storage, jika kosong buat array baru
+    //Mengambil data session storage, jika kosong buat array baru
     let guests = JSON.parse(sessionStorage.getItem('guests')) || [];
 
     guests.forEach(guest => {
-        let li = document.createElemet('li');
+        let li = document.createElement('li');
         li.textContent = guest;
         guestListUI.appendChild(li);
     });
 }
 
-//Fungsi untuk menambah data
 function addGuest(){
     const input = document.getElementById('guestInput');
     const guestName = input.value;
-    if (guestName === ''){
-        alert('Nama tidak boleh kosong! ');
+    if(guestName === ''){
+        alert('Nama tidak boleh kosong!');
         return;
     }
-
-    //Ambil data lama
+    //ambil data lama
     let guests = JSON.parse(sessionStorage.getItem('guests')) || [];
     //Tambah data baru ke array
     guests.push(guestName);
 
-    //Simpan kembali ke storage dalam bentuk String
+    //simpan kembali ke storage dalam bentuk string
     sessionStorage.setItem('guests', JSON.stringify(guests));
     //Bersihkan input dan perbarui tampilan
     input.value = '';
-    displayGuest();
+    displayGuests();
 }
 
-//Fungi untuk menghapus semua data 
-function clearList() {
-    if (confirm('Hapus semua data tamu di sesi ini?')) {
+function clearList(){
+    if(confirm('Hapus semua data tamu di sesi ini?')){
         sessionStorage.removeItem('guests');
-        displayGuest();
+        displayGuests();
     }
 }
 
-//Jalankan fungsi display saat halaman pertama kali dimuat
-window.onload = displayGuest;
+window.onload = displayGuests;
